fbdb500045b18d46f808a293873df4710c23ebef
kravindra1415 <kravindra1415@gmail.com>
2022-08-19 18:54:07 +0530

    All done

---
+++ src/app/main/customer/create-customer-modal.component.ts
@@ -0,0 +1,81 @@
+import { Component, ViewChild, Injector, ElementRef, Output, EventEmitter, OnInit } from '@angular/core';
+import { CreateCustomerInput, CustomerServiceProxy, User } from '@shared/service-proxies/service-proxies';
+import { AppComponentBase } from '@shared/common/app-component-base';
+import { finalize } from 'rxjs/operators';
+import { ModalDirective } from 'ngx-bootstrap/modal/modal.directive';
+@Component({
+    selector: 'createCustomerModal',
+    templateUrl: './create-customer-modal.component.html'
+})
+export class CreateCustomerModalComponent extends AppComponentBase implements OnInit {
+
+    filter: string = '';
+    user: User[] = [];
+    @Output() modalSave: EventEmitter<any> = new EventEmitter<any>();
+
+    @ViewChild('modal', { static: false }) modal: ModalDirective;
+    @ViewChild('nameInput', { static: false }) nameInput: ElementRef;
+    userValues = [];
+    customer: CreateCustomerInput = new CreateCustomerInput();
+
+    active: boolean = false;
+    saving: boolean = false;
+
+    constructor(
+        injector: Injector,
+        private _customerService: CustomerServiceProxy
+    ) {
+        super(injector);
+    }
+    ngOnInit(): void {
+        this.userValues = [];
+        this.getUser();
+    }
+
+    getUser() {
+        this._customerService.getUsers(this.filter).subscribe((result) => {
+            this.user = result.items;
+            console.log("user=", this.user);
+        });
+    }
+    show(): void {
+        this.active = true;
+        this.customer = new CreateCustomerInput();
+        this.modal.show();
+    }
+
+    onShown(): void {
+        this.nameInput.nativeElement.focus();
+    }
+
+    save(): void {
+        this.saving = true;
+        this._customerService.createCustomer(this.customer)
+            .pipe(finalize(() => this.saving = false))
+            .subscribe(() => {
+                this.notify.info(this.l('SavedSuccessfully'));
+                this.close();
+                this.modalSave.emit(this.customer);
+            });
+    }
+
+    refresh(): void {
+        window.location.reload();
+    }
+
+    onselect(value) {
+        console.log("selected id=", value)
+    }
+    pushValue(value) {
+
+        console.log("userIds=", value);
+        this.userValues.push(value);
+        console.log("UserValues=", this.userValues);
+        this.customer.userRefId = this.userValues;
+    }
+    close(): void {
+        this.modal.hide();
+        this.active = false;
+    }
+
+}
---
+++ src/app/main/customer/customer.component.ts
@@ -0,0 +1,220 @@
+import { Component, Injector, OnInit } from '@angular/core';
+import { AppComponentBase } from '@shared/common/app-component-base';
+import { appModuleAnimation } from '@shared/animations/routerTransition';
+import { AddUserInput, CustomerListDto, CustomerServiceProxy, User, UserViewDto } from '@shared/service-proxies/service-proxies';
+import * as _ from 'lodash-es';
+@Component({
+    templateUrl: './customer.component.html',
+    animations: [appModuleAnimation()]
+})
+
+export class CustomerComponent extends AppComponentBase implements OnInit {
+    customer: CustomerListDto[] = [];
+    filter: string = '';
+    filter2: string = '';
+    editingCustomer: CustomerListDto = null;
+    newUser: AddUserInput = null;
+    user: User[] = [];
+    custUser: UserViewDto[] = [];
+    isDeleted = [];
+    check = false;
+    parentSelect: boolean = false;
+
+    constructor(
+        injector: Injector,
+        private _customerService: CustomerServiceProxy
+    ) {
+        super(injector);
+    }
+
+    ngOnInit(): void {
+        this.getCustomer();
+        this.getUser();
+    }
+
+    getCustomer(): void {
+        this._customerService.getCustomer(this.filter).subscribe((result) => {
+            this.customer = result.items;
+        });
+    }
+    deleteCustomer(customer: CustomerListDto): void {
+        this.message.confirm(
+            this.l('If yes click Yes button', customer.customerName),
+            this.l('Are You Sure To Delete The Person?'),
+            isConfirmed => {
+                if (isConfirmed) {
+                    this._customerService.deleteCustomer(customer.id).subscribe(() => {
+                        this.notify.info(this.l('SuccessfullyDeleted'));
+                        _.remove(this.customer, customer);
+                    });
+                }
+            }
+        );
+    }
+    editCustomer(customer: CustomerListDto): void {
+        if (customer === this.editingCustomer) {
+            this.editingCustomer = null;
+        } else {
+            this.editingCustomer = new CustomerListDto();
+            this.editingCustomer.custUsers = [];
+            this.editingCustomer = customer;
+
+            this.newUser = new AddUserInput();
+
+            this.newUser.customerRefId = customer.id;
+        }
+    };
+
+    deleteUser(user, customer): void {
+        this._customerService.deleteUser(user.id).subscribe(() => {
+            this.notify.success(this.l('SuccessfullyDeleted'));
+            _.remove(customer.custUsers, user);
+        });
+    };
+
+    saveUser(): void {
+        if (!this.newUser.customerRefId && !this.newUser.userRefId) {
+            this.message.warn('Please enter a number!');
+            return;
+        }
+        this._customerService.addUser(this.newUser).subscribe((result) => {
+            this.editingCustomer.custUsers.push(result);
+            console.log(result);
+            this.notify.success(this.l('SavedSuccessfully'));
+        });
+    };
+    getUser() {
+        this._customerService.getUsers(this.filter2).subscribe((result) => {
+            this.user = result.items;
+            console.log("user=", this.user);
+        });
+    }
+    viewUser(customerId) {
+        this._customerService.getUserView(customerId).subscribe((result: any) => {
+            console.log("custuser=", result);
+            this.custUser = result;
+        })
+    }
+
+    // value(val: number) {
+    //     // console.log("val= ", val);
+    //     // var u = this.isDeleted.push(val);
+    //     // console.log("u=", u);
+    //     // return u;
+
+    //     if (this.check) {
+    //         var u = this.isDeleted.push(val);
+    //         console.log("u=", u)
+    //         return u;
+    //     }
+    //     else {
+    //         return this.isDeleted.splice(val)
+    //     }
+    // }
+
+    value($event) {
+        const id = $event.target.value;
+        const idChecked = $event.target.checked;
+        console.log(id, idChecked);
+
+        this.customer = this.customer.map((d) => {
+            if (d.id == id) {
+                d.selected = idChecked;
+                this.parentSelect = false;
+                this.isDeleted.push(d.id);
+                return d;
+            }
+
+            if (id == -1) {
+                d.selected = this.parentSelect;
+                //this.isDeleted.push(d.id);
+                return d;
+            }
+            return d;
+        })
+        console.log(this.customer);
+
+        //     if(!this.check)
+
+        //     {
+
+        //         console.log("val= ",val);
+
+        //         var u= this.isDeleted.push(val);
+
+        //         console.log("u=",u);
+
+        //         return u;
+
+        //     }
+
+        //     else{
+
+        //    var e= this.isDeleted.pop();
+
+        //    console.log("e=",e);
+
+        //    return e;
+
+        //     }
+
+    }
+
+
+    // deleteCustomerSelected() {
+    //     console.log(this.isDeleted);
+    //     this._customerService.deleteMultipleCustomer(this.isDeleted).subscribe(() => {
+    //         this.notify.info(this.l('SuccessfullyDeleted'));
+    //         this.isDeleted.forEach(element => {
+    //             _.remove(this.customer, element);
+    //         });
+    //     });
+    //     window.location.reload();
+    // }
+
+    deleteCustomerSelected() {
+        console.log("customer=", this.customer);
+        this._customerService.deleteMultipleCustomer(this.isDeleted).subscribe(() => {
+            this.isDeleted.forEach(element => {
+                console.log("selected", element.selected);
+                if (element.selected == true) {
+                    _.remove(this.customer, element);
+                }
+            })
+        });
+
+        // console.log(this.isDeleted);
+
+        // this._customerService.deleteMultipleCustomer(this.isDeleted).subscribe(() => {
+
+        //     this.notify.info(this.l('SuccessfullyDeleted'));
+
+        //     this.isDeleted.forEach(element => {
+
+        //         _.remove(this.customer, element);
+
+        //     });
+
+        // });
+
+
+        // this.customer= this.customer.filter(_ => _.selected);
+
+        //     for (var food in this.customer) {
+
+        //  this._customerService.deleteMultipleCustomer(this.customer)
+
+        //      .subscribe(data =>{
+
+        //       console.log(data)
+
+        //      }  
+
+        //      )    
+
+        //   }
+
+        //window.location.reload();
+
+    }
+}
